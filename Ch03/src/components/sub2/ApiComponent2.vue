124f161c8bf783f3bb30f846a1e27ca78024ac1a
Hwangwonjin <ghkddnjs1269@gmail.com>
2023-10-17 21:49:59 +0900

    Ch03

---
+++ Ch03/src/components/sub2/ApiComponent2.vue
@@ -0,0 +1,27 @@
+<template>
+    <h4>method</h4>
+    <button v-on:click="btn1Click">button1</button>
+    <button v-on:click="btn2Click">button2</button>
+    <button @click="btn3Click">button3</button>
+</template>
+<script>
+export default {
+    name:"ApiComponent2",
+    methods: {
+        btn1Click(){
+            alert("button1 click!");
+        },
+
+        btn2Click: function(){
+            alert("button2 click!");
+        },
+
+        btn3Click: () => {
+            alert("button3 Click!");
+        },
+    },
+};
+</script>
+<style scoped>
+    
+</style>
---
+++ Ch03/src/components/sub3/UComponent1.vue
@@ -0,0 +1,19 @@
+<template>
+    <h4>UComponent1</h4>
+    <p>
+        name: {{ name }}<br/>
+        age: {{ age }}<br/>
+    </p>
+</template>
+
+<script>
+export default {
+    name:"UComponent1",
+    // 상위 컴포넌트에서 속성으로 전갈하는값을 수신하기 위한 api
+    props:["name", "age"],
+};
+</script>
+
+<style scoped>
+    
+</style>
